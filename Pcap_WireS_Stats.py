d6964b1c5921c8595500e2fbdad5b9d70ee87abb
Fviramontes8 <kuolemajarvi@yahoo.com>
2017-02-22 09:45:15 -0700

    Add files via upload

--- Pcap_WireS_Stats.py
+++ Pcap_WireS_Stats.py
@@ -1,13 +1,15 @@
 from glob import glob
-import math
 from pcapfile import savefile
 from pcapfile.protocols.linklayer import ethernet
+from pcapfile.protocols.network import ip
 import datetime
+import binascii
+import socket
 
 
 for file_name in glob("*.pcap"):
-    with open(file_name, "rb") as pcap_data:      #Opens .json files in working directory
-        pdata = savefile.load_savefile(pcap_data, verbose=True)#Loads .json data into variable "data" 
+    with open(file_name, "rb") as pcap_data:      #Opens .pcap files in working directory
+        pdata = savefile.load_savefile(pcap_data, verbose=True)#Loads .pcap data into variable "pdata" 
         uniqueEth = []                       #Empty list of Unique MAC addresses
         nOU= 0                               #Number of Users
         cBits = 0                            #Cumulitive Bits
@@ -15,42 +17,52 @@ for file_name in glob("*.pcap"):
                                              # address, destination MAC addresses
                                              # and bits from source to destination
         
-        for n in range(0, len(pdata)):
+        for n in range(0, len(pdata.packets)):
+            pkt = pdata.packets[n]
+            #Timestamp in UTC
+            t_stamp = str(datetime.datetime.utcfromtimestamp(pkt.timestamp))
+            print t_stamp
+            eth_frame = ethernet.Ethernet(pdata.packets[n].raw())            
+            #Source MAC address
+            eth_src = eth_frame.src
+            #Destination MAC address
+            eth_dst = eth_frame.dst
             #Amount of bits from the nth packet
-            dBits = int(data[n]["_source"]["layers"]["frame"]\
-            ["frame.len"]) * 8
+            dBits = pkt.packet_len * 8
             #Simple calculation of cumulitive bits
             cBits += dBits
-            #Source MAC address
-            data_src = str(data[n]["_source"]["layers"]["eth"]["eth.src"])
-            #Destination MAC address
-            data_dst = str(data[n]["_source"]["layers"]["eth"]["eth.dst"])
-            #Computes bandwidth
-            cBand = math.trunc(cBits / float(data[len(data) - 1]["_source"]["layers"]\
-            ["frame"]["frame.time_relative"]))
 
             #Adds source and destination MAC address and the number of bits
             # from source to destination into a dictionary, modeled like this:
             # {src1 :{dst1 : bits from src1 to dst1}, {dst2 : bits from src1 to dst2}}   
-            if data_src not in ethDict:             #Adds to dictionary with initial bits
-                ethDict[data_src] = {data_dst : dBits}
-            elif data_dst not in ethDict[data_src]: #Adds additional destinations
-                ethDict[data_src].update({data_dst : dBits})
+            if eth_src not in ethDict:             #Adds to dictionary with initial bits
+                ethDict[eth_src] = {eth_dst : dBits}
+            elif eth_dst not in ethDict[eth_src]: #Adds additional destinations
+                ethDict[eth_src].update({eth_dst : dBits})
             else:                                   #Updates bits in dictionary
-                ethDict[data_src][data_dst] += dBits
+                ethDict[eth_src][eth_dst] += dBits
         
             #This adds all unique MAC addresses to a list
-            if data_src not in uniqueEth:
-                uniqueEth.append(data_src)
+            if eth_src not in uniqueEth:
+                uniqueEth.append(eth_src)
                 nOU += 1
-            if data_dst not in uniqueEth:
-                uniqueEth.append(data_dst)
+            if eth_dst not in uniqueEth:
+                uniqueEth.append(eth_dst)
                 nOU += 1
-
+                
+        
+            ip_frame = ip.IP(binascii.unhexlify(eth_frame.payload))
+            ip_src = ip_frame.src
+            ip_dst = ip_frame.dst
+            host = socket.getfqdn(ip_src)
+            print host
+            host = socket.getfqdn(ip_dst)
+            print host
+            
         print "Number of Users: "+str(nOU)
         for z in uniqueEth:
             print z
-        print "\nBandwidth is "+str(cBand)+" bits/s \n"
+        print "\nBandwidth is "+str(cBits)+" bits/s \n"
         for k in ethDict: 
             for n in ethDict[k]:
                 print "From " + str(k) + " to " + str(n)+" is " +\
