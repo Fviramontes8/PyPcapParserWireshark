3b8670a9fce308015f8e7cd48f25a7cbff513dd3
Seth Decker <sdeck51@unm.edu>
2017-05-12 15:54:13 -0600

    Create DatabaseConnect.cpp

---
+++ DatabaseConnect.cpp
@@ -0,0 +1,43 @@
+#include "DatabaseConnect.hpp"
+
+DatabaseConnect::DatabaseConnect()
+{
+	
+}
+DatabaseConnect::DatabaseConnect(std::string _databasename, std::string _host, std::string _username, std::string _password)
+{
+	databasename = _databasename;
+	username = _username;
+	password = _password;
+	host = _host;
+	
+}
+
+int DatabaseConnect::connect()
+{
+	std::string conninfo =  "dbname=" + username + " " + " host=" + host + " " + " user=" + username + " " + " password=" + password;
+	std::cout << conninfo << std::endl;
+	conn = PQconnectdb(conninfo.c_str());
+	    /* Check to see that the backend connection was successfully made */
+    if (PQstatus(conn) != CONNECTION_OK)
+    {
+        fprintf(stderr, "Connection to database failed: %s",
+                PQerrorMessage(conn));
+    }
+	return 0;
+}
+
+int DatabaseConnect::writeData(/*list here*/)
+{
+	/*loop and format data*/
+	PQexec(conn, "INSERT INTO cplusplustest VALUES('burp2', 100000, 100000)");
+	printf("%s\n", PQerrorMessage(conn));
+	return 0;
+}
+
+int DatabaseConnect::disconnect()
+{
+	PQfinish(conn);
+	
+	return 0;
+}
