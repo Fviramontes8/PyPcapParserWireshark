cf3787683add2f2bb35c7f8674f5ba1e4748ec1b
Seth Decker <sdeck51@unm.edu>
2017-05-12 15:56:31 -0600

    Create DatabaseConnectDriver.cpp

---
+++ DatabaseConnectDriver.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "DatabaseConnect.hpp"
+
+
+
+int main() {
+	std::string dbname = "postgres";
+	std::string hostname = "129.24.26.136";
+	std::string username = "postgres";
+	std::string password = "postgresUNM";
+
+	std::cout << hostname << std::endl;
+	DatabaseConnect db(dbname, hostname, username, password);
+	//DatabaseConnect db();
+	
+	db.connect();
+	
+	db.writeData();
+	
+	db.disconnect();
+}
