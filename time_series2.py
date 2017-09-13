88fd132dbbfa8b01aeed85b7ff3d4d168a1c9226
Francisco Octavio Viramontes <fviramontes8@unm.edu>
2017-09-13 16:36:59 -0600

    GP is now working properly

--- time_series2.py
+++ time_series2.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 #from random import seed
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.metrics.pairwise import linear_kernel
 #, ConstantKernel as CK
 #, Matern
 #from sklearn.gaussian_process.kernels import RationalQuadratic as RQ, ExpSineSquared as ESS 
@@ -67,7 +68,7 @@ def avg_sample(sample_arr, sample_size):
                 m.append(a[i])
             sample_return.append(int(mean(m)))
             break
-    return sample_return
+    return np.atleast_1d(sample_return)
 
 def grab_n(array, length):
     return np.atleast_1d([array[i] for i in range(length)]).T
@@ -77,6 +78,7 @@ def grab_nz(array, n ,z):
 #Main:
 timestamps = []
 nou = []
+nou_tst = []
 bits = []
 pktNum = []
 sigS = []
@@ -90,6 +92,7 @@ db.connect()
 
 #Need to do recapture of wed
 train = db.readTable("mon")
+test = db.readTable("tues")
 #db.writeDataTable("pcap_6h")
 
 db.disconnect()
@@ -105,6 +108,8 @@ for k in sorted(train, key=lambda hello: hello[0]):
     phyB.append(int(k[7]))
     phyG.append(int(k[8]))
     phyN.append(int(k[9]))
+for l in sorted(test, key=lambda yello: yello[0]):
+    nou_tst.append(int(l[2]))
 '''  
 human_time = []
 for u in timestamps:
@@ -126,71 +131,89 @@ plt.show()
 print "Average number of users: " + str(int(mean(nou)))
 print "Standard deviation: " + str(int(np.sqrt(sample_var(nou, mean(nou)))))
 
-#Trying grabbing the first ten to predict 11th
-m = 0
-n = 10
-while n < 20:
-    print m+1
-    samp_Nou = grab_nz(nou, m, n)
-    #sub_sample(nou, 3600)
-    #avg_sample(nou, 3600)
-    samp_Bits = grab_nz(bits, m, n)
-    #sub_sample(bits, 3600)
-    #avg_sample(bits, 3600)
-    samp_Pkts = grab_nz(pktNum, m, n)
-    #sub_sample(pktNum, 3600)
-    #avg_sample(pktNum, 3600)
-    samp_sigS = grab_nz(sigS, m, n)
-    #sub_sample(sigS, 3600)
-    #avg_sample(sigS, 3600)
-    samp_dR = grab_nz(dataRate, m, n)
-    #sub_sample(dataRate, 3600)
-    #avg_sample(dataRate, 3600)
-    samp_pB = grab_nz(phyB, m, n)
-    #sub_sample(phyB, 3600)
-    #avg_sample(phyB, 3600)
-    samp_pG = grab_nz(phyG, m, n)
-    #sub_sample(phyG, 3600)
-    #avg_sample(phyG, 3600)
-    samp_pN = grab_nz(phyN, m, n)
-    #sub_sample(phyN, 3600) 
-    #avg_sample(phyN, 3600)
-    
-    y = np.atleast_2d([samp_Nou, samp_Bits, samp_Pkts, samp_sigS, samp_dR,\
-                       samp_pB, samp_pG, samp_pN]).T
-    nu = y.shape[0]
-    
-    X = np.atleast_2d([[i for i in range(nu)]]).T
-    x =  np.atleast_2d([[i for i in range(nu, nu + 1)]]).T
-    
-    
-    kernel = RBF(length_scale=1, length_scale_bounds=(1e-1, 1e1))
-    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10,\
-                                  normalize_y=True)
-    #print X
-    #print y
-    gp.fit(X, y)
-    
-    print "Marginal likelihood:", gp.log_marginal_likelihood()
-    
-    #print x
-    y_p = gp.predict(X)
-    #print y_p
-    
-    y_p1, sigma_p1 = gp.predict(x, return_std=True)
-    print "Means:\t\t", [int(mean(samp_Nou)), int(mean(samp_Bits)),\
-                       int(mean(samp_Pkts)), int(mean(samp_sigS)),\
-                       int(mean(samp_dR)), int(mean(samp_pB)),\
-                       int(mean(samp_pG)), int(mean(samp_pN))]
-    print "Predicted:\t", [int(y_p1[0][0]), int(y_p1[0][1]), int(y_p1[0][2]),\
-                          int(y_p1[0][3]), int(y_p1[0][4]), int(y_p1[0][5]),\
-                          int(y_p1[0][6]), int(y_p1[0][7])]
-    
-    print "Real:\t\t", [nou[nu+m], bits[nu+m], pktNum[nu+m], sigS[nu+m],\
-                      dataRate[nu+m], phyB[nu+m], phyG[nu+m], phyN[nu+m]], "\n"
-    m += 1
-    n += 1
+#
 
+#samp_ts = sub_sample(timestamps, 3600)
+samp_Nou = avg_sample(nou, 3600)
+samp_nou_tst = avg_sample(nou_tst, 3600)
+#sub_sample(nou, 3600)
+#grab_nz(nou, m, n)
+samp_Bits = avg_sample(bits, 3600)
+#sub_sample(bits, 3600)
+#grab_nz(bits, m, n)
+samp_Pkts = avg_sample(pktNum, 3600)
+#sub_sample(pktNum, 3600)
+#grab_nz(pktNum, m, n)
+samp_sigS = avg_sample(sigS, 3600)
+#sub_sample(sigS, 3600)
+#grab_nz(sigS, m, n)
+samp_dR = avg_sample(dataRate, 3600)
+#sub_sample(dataRate, 3600)
+#grab_nz(dataRate, m, n)
+samp_pB = avg_sample(phyB, 3600)
+#sub_sample(phyB, 3600)
+#grab_nz(phyB, m, n)
+samp_pG = avg_sample(phyG, 3600)
+#sub_sample(phyG, 3600)
+#grab_nz(phyG, m, n)
+samp_pN = avg_sample(phyN, 3600)
+#sub_sample(phyN, 3600) 
+#grab_nz(phyN, m, n)
+
+#y = np.atleast_2d([samp_Nou, samp_Bits, samp_Pkts, samp_sigS, samp_dR,\
+                   #samp_pB, samp_pG, samp_pN]).T
+#nu = y.shape[0]
+print samp_Nou
+print samp_nou_tst
+D = 10 #Window size
+
+Xtr = np.atleast_2d([grab_nz(samp_Nou, m, n) for m, n in zip(range(D+1), range(D,samp_Nou.shape[0]))]) #Training
+print Xtr
+ytr = np.atleast_2d([[samp_Nou[i] for i in range(D,samp_Nou.shape[0])]]).T #Observations
+print ytr
+
+xtst =  np.atleast_2d([grab_nz(samp_nou_tst, m, n) for m, n in zip(range(D+1), range(D,samp_nou_tst.shape[0]))]) #Test
+print xtst
+ytst = np.atleast_2d([samp_nou_tst[i] for i in range(D,samp_nou_tst.shape[0])]).T
+print ytst.T[0]
+#Column features, rows samples
+
+kernel = RBF(length_scale=1, length_scale_bounds=(1e-1, 1e1))
+#linear_kernel(X, y)
+gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10,\
+                              normalize_y=True)
+
+gp.fit(Xtr, ytr)
+
+print "Marginal likelihood:", gp.log_marginal_likelihood()
+
+y_p = gp.predict(xtst)
+print y_p.T[0]
+
+tues_hrs = [i+1 for i in range(D, samp_nou_tst.shape[0])]
+print tues_hrs
+
+plt.xlabel("Time of day (hrs)")
+plt.ylabel("Number of Users")
+plt.plot(tues_hrs, (y_p.T)[0], "g-", label="Predicted")
+plt.plot(tues_hrs, ytst.T[0], "m-", label="Real")
+plt.legend()
+plt.show()
+'''
+y_p1, sigma_p1 = gp.predict(x, return_std=True)
+
+print "Means:\t\t", [int(mean(samp_Nou)), int(mean(samp_Bits)),\
+                   int(mean(samp_Pkts)), int(mean(samp_sigS)),\
+                   int(mean(samp_dR)), int(mean(samp_pB)),\
+                   int(mean(samp_pG)), int(mean(samp_pN))]
+print "Predicted:\t", [int(y_p1[0][0]), int(y_p1[0][1]), int(y_p1[0][2]),\
+                      int(y_p1[0][3]), int(y_p1[0][4]), int(y_p1[0][5]),\
+                      int(y_p1[0][6]), int(y_p1[0][7])]
+
+print "Real:\t\t", [nou[nu+m], bits[nu+m], pktNum[nu+m], sigS[nu+m],\
+                  dataRate[nu+m], phyB[nu+m], phyG[nu+m], phyN[nu+m]], "\n"
+
+'''
 '''
 print (y_p.T)
 
