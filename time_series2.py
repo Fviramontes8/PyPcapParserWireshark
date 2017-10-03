bbbe9f3c8770dacad3cba18c8dce7369c54e1cb4
Francisco Octavio Viramontes <fviramontes8@unm.edu>
2017-10-03 17:03:34 -0600

    Code gives some features with larger variance

--- time_series2.py
+++ time_series2.py
@@ -12,12 +12,17 @@ np.set_printoptions(threshold=np.nan)
 import matplotlib.pyplot as plt
 #from random import seed
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import DotProduct as LK, ConstantKernel as CK, Sum
-#sig_b**2 + (sig_v**2)*(x-c)(xprime-c)
-#from sklearn.metrics.pairwise import linear_kernel
-#, ConstantKernel as CK
-#, Matern
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import DotProduct as LK, WhiteKernel as WK
+from sklearn.gaussian_process.kernels import ConstantKernel as CK, Sum
 #from sklearn.gaussian_process.kernels import RationalQuadratic as RQ, ExpSineSquared as ESS 
+
+
+class MyError(Exception):
+     def __init__(self, value):
+         self.value = value
+     def __str__(self):
+         return repr(self.value)
 
 def mean(values):
     return sum(values) / float(len(values))
@@ -77,25 +82,18 @@ def grab_nz(array, n ,z):
     return np.atleast_1d([array[i] for i in range(n, z)]).T
 #(nou, nou_tst, 60, 0, 100, 15)
 def GP_prep(train, test, avg_samp, sub_samp_begin, sub_samp_end, window):
-    samp_train = avg_sample(train, avg_samp) #samp_nou
+    samp_train = avg_sample(train, avg_samp)
     tot_samp = len(samp_train)
-    #noutr = np.atleast_2d([grab_nz(samp_nou, m, n) for m, n in zip(range(samp_nou.shape[0]), range(D,samp_nou.shape[0]))])
     tr = np.atleast_2d([grab_nz(samp_train, m, n) for m, n in zip(range(samp_train.shape[0]), range(window, samp_train.shape[0]))])
     Xtr = np.atleast_2d(tr)
-    #nou_ob = np.atleast_2d([[samp_nou[i] for i in range(D, samp_nou.shape[0])]])
     ob = np.atleast_2d([[samp_train[i] for i in range(window, samp_train.shape[0])]])
     Ytr = np.atleast_2d(ob).T 
-    #samp_nou_tst = avg_sample(nou_tst, 60)
     samp_test = avg_sample(test, avg_samp) 
-    #samp_nou_tst1 = grab_nz(samp_nou_tst, p, r)
     samp_test1 = grab_nz(samp_test, sub_samp_begin, sub_samp_end) 
-    ##nou_tst1 = [grab_nz(samp_nou_tst1, m, n) for m, n in zip(range(samp_nou_tst1.shape[0]), range(D,samp_nou_tst1.shape[0]))]
     feat_xtest = [grab_nz(samp_test1, m, n) for m, n in zip(range(samp_test1.shape[0]), range(window, samp_test1.shape[0]))]
     xtst =  np.atleast_2d(feat_xtest)
-    #nou_comp = [samp_nou[i] for i in range(D,samp_nou_tst1.shape[0])]
     feat_comp = [samp_train[i] for i in range(window, samp_test1.shape[0])]
     ycomp = np.atleast_2d(feat_comp).T
-    #nou_ytst = [samp_nou_tst[i] for i in range(D,samp_nou_tst1.shape[0])]
     feat_ytest = [samp_test[i] for i in range(window, samp_test1.shape[0])]
     ytst = np.atleast_2d(feat_ytest).T
     return tot_samp, Xtr, Ytr, xtst, ycomp, ytst
@@ -172,110 +170,59 @@ plt.show()
 
 training_data=[nou, bits, pktNum, sigS, dataRate, phyB, phyG, phyN]
 test_data = [nou_tst, bits_tst, pkt_tst, sigS_tst, dR_tst, b_tst, g_tst, n_tst]
-labels = {"nou" : "Number of users", "bits" : "Bits", "pktNum": "Number of Packets",\
-          "sigS": "Signal Strength", "dataRate": "Data Rate(MB)", "phyB": "802.11b bits",\
-          "phyG": "802.11g bits", "phyN": "802.11n bits"}
-samp_nou = avg_sample(nou, 60)
-q = len(samp_nou)
-
-#Preparation of subsampling test data
-samp_nou_tst = avg_sample(nou_tst, 60)
+labels = ["Number of users", "Bits", "Number of Packets", "Signal Strength",\
+          "Data Rate(MB)", "802.11b bits", "802.11g bits", "802.11n bits"]
+#
 #Number of test samples
 p = 0
 r = 100
-#Subsampling features
-samp_nou_tst1 = grab_nz(samp_nou_tst, p, r)
-
 #Window size
 D = 15
 
-#Column features, rows samples
-#Training samples
-noutr = np.atleast_2d([grab_nz(samp_nou, m, n) for m, n in zip(range(samp_nou.shape[0]), range(D,samp_nou.shape[0]))])
-Xtr = np.atleast_2d(noutr) #Training
-#print "Xtr:\n", Xtr
-print len(Xtr)
-
-#Training Observations
-nou_ob = np.atleast_2d([[samp_nou[i] for i in range(D, samp_nou.shape[0])]])
-ytr = np.atleast_2d(nou_ob).T 
-#print "ytr:\n", ytr
-
-#Test samples
-nou_tst1 = [grab_nz(samp_nou_tst1, m, n) for m, n in zip(range(samp_nou_tst1.shape[0]), range(D,samp_nou_tst1.shape[0]))]
-xtst =  np.atleast_2d(nou_tst1) 
-#print "xtst:\n", xtst
-
-#Comparison
-nou_comp = [samp_nou[i] for i in range(D,samp_nou_tst1.shape[0])]
-ycompare = np.atleast_2d(nou_comp).T
-
-#Test Observations
-nou_ytst = [samp_nou_tst[i] for i in range(D,samp_nou_tst1.shape[0])]
-ytst = np.atleast_2d(nou_ytst).T
-#print "ytst:\n", ytst.T[0]
-
 
 kernel1 = LK(sigma_0 = 1, sigma_0_bounds = (1e-1, 1e1))
-kernel2 = CK(constant_value=1.0)
+kernel2 = CK(constant_value=1)
+kernel3 = WK(1)
 kernel = Sum(kernel1, kernel2)
+kernel = Sum(kernel, kernel3)
+#1e-1 for linear + constant, 1e-3 for RBF
 gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10,\
-                              normalize_y=True, alpha=1e-6)
+                              normalize_y=False, alpha=1e-1)
+print gp.get_params()['kernel']
 
-gp.fit(Xtr, ytr)
-
-print "Marginal likelihood:", gp.log_marginal_likelihood()
-
-Xtr_1 = [Xtr[i] for i in range(xtst.shape[0])]
-
-y_p, y_sig = gp.predict(Xtr_1, return_std=True)
-#print y_p.T[0]
-#print y_sig
-
-result_time = [i+1 for i in range(D, samp_nou_tst1.shape[0])]
-
-#Axes and graph titling 
-s = "Time interval between "+str(result_time[0])+" and "+str(result_time[-1])+\
-" minutes\n Window is "+str(D)
-plt.xlabel(s=s)
-yLab = "Number of packets"
-plt.ylabel(yLab)
-o = "Using RBF Kernel with "+str(q)+" averaged training samples\nand "+str(r)+\
-" averaged test samples"
-plt.title(s=o)
-
-#Ploting data
-plt.plot(result_time, ycompare, "y-", label="Training")
-plt.plot(result_time, y_p.T[0], "g-", label="Predicted")
-#plt.plot(result_time, ytst.T[0], "m-", label="Real")
-plt.fill(np.concatenate([result_time, result_time[::-1]]),
-         np.concatenate([y_p-1.96*y_sig,
-                        (y_p+1.96*y_sig)[::-1]]),
-         alpha=.5, fc='b', ec='None')
-plt.legend()
-plt.show()
-
-total_samp, Xtr, Ytr, Xtst, Ycomp, Ytst = GP_prep(nou, nou_tst, 60, p, r, D)
-
-gp.fit(Xtr, Ytr)
-y_pred, y_sigma = gp.predict(Xtst, return_std=True)
-
-s = "Time interval between "+str(result_time[0])+" and "+str(result_time[-1])+\
-" minutes\n Window is "+str(D)
-plt.xlabel(s=s)
-yLab = "Number of users"
-plt.ylabel(yLab)
-o = "Using RBF Kernel with "+str(q)+" averaged training samples\nand "+str(r)+\
-" averaged test samples"
-plt.title(s=o)
-
-#Ploting data
-plt.plot(result_time, Ycomp, "y-", label="Training")
-plt.plot(result_time, y_pred.T[0], "g-", label="Predicted")
-#plt.plot(result_time, ytst.T[0], "m-", label="Real")
-plt.fill(np.concatenate([result_time, result_time[::-1]]),
-         np.concatenate([y_pred-1.96*y_sigma,
-                        (y_pred+1.96*y_sigma)[::-1]]),
-         alpha=.5, fc='b', ec='None')
-plt.legend()
-plt.show()
+for z in range(len(labels)): #len(labels)
+    total_samp, Xtr, Ytr, Xtst, Ycomp, Ytst = GP_prep(training_data[z], test_data[z], 60, p, r, D)
+    
+    #Testing if it overfits
+    Xtr_1 = [Xtr[i] for i in range(D, r)]
+    
+    #y_sample_yo = gp.sample_y(Xtr_1, 1)
+    try:
+        gp.fit(Xtr, Ytr)
+        print "marginal likelihood:", gp.log_marginal_likelihood()
+        y_pred, y_sigma = gp.predict(Xtst, return_std=True)
+        result_time = [g+1 for g in range(D, r)]
+        
+        s = "time interval between "+str(result_time[0])+" and "+str(result_time[-1])+\
+        " minutes\n window is "+str(D)
+        plt.xlabel(s=s)
+        ylab = labels[z]
+        plt.ylabel(ylab)
+        o = "Using "+str(gp.get_params()['kernel'])+" kernel\nwith "+str(total_samp)+" averaged training samples\nand "+str(r)+\
+        " averaged test samples"
+        plt.title(s=o)
+        
+        #ploting data
+        #plt.plot(result_time, y_sample_yo, "c-", label= "kernel sample")
+        #plt.plot(result_time, ycomp, "y-", label="training")
+        plt.plot(result_time, y_pred.T[0], "g-", label="predicted")
+        plt.plot(result_time, Ytst.T[0], "m-", label="real")
+        plt.fill(np.concatenate([result_time, result_time[::-1]]),
+                 np.concatenate([y_pred-1.96*y_sigma,
+                                (y_pred+1.96*y_sigma)[::-1]]),
+                 alpha=.5, fc='b', ec='none')
+        plt.legend()
+        plt.show()
+    except:
+        print "Feature "+str(labels[z])+" did not work!"
+        continue
