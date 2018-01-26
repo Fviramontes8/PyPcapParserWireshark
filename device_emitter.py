1c769e059dfc1ca746f3b538b19d8d31031b115a
root <root@localhost.localdomain>
2018-01-26 14:21:03 -0700

    device emitter is updated

--- device_emitter.py
+++ device_emitter.py
@@ -7,7 +7,6 @@ Created on Wed Jan 24 15:17:49 2018
 
 import DatabaseConnect as dc
 
-
 db = dc.DatabaseConnect()
 db.connect()
 table_contents = db.readTable("ip")
@@ -17,4 +16,25 @@ db.disconnect()
 pi_details = {}
 for i in table_contents:
     pi_details[i[0]] = [i[1], i[2]]
-print(pi_details)
+#print(pi_details)
+
+text_object = open("ip.txt", "r")
+#print(text_object.read())
+ip_address = text_object.read()
+text_object.close()
+ip_address = ip_address.strip("\n")
+#print("This computer's IP address: " + str(ip_address))
+
+text_object = open("mac.txt", "r")
+mac_address = text_object.read()
+text_object.close()
+mac_address = mac_address.strip("\n")
+#print("This computer's MAC address: " + mac_address)
+
+for pi_iterator in range(len(pi_details)):
+    database_mac = pi_details[pi_iterator+1][0]
+    #print("Database: " + database_mac)
+    if(database_mac == mac_address):
+        print("There is a match, here is the key: " +str(pi_iterator+1))
+    else:
+        print("There is no match!")
